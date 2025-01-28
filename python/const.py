7d5a7b6d4ff87b69725ed50c838fae10427a744e
Will Roscoe <w.roscoe@lancaster.ac.uk>
2025-01-28 13:30:57 +0000

    removed test function call

--- python/const.py
+++ python/const.py
@@ -5,5 +5,7 @@ GHROOT = Path(__file__).parents[1]
 # takes a relative path within repo and returns an absolute path.
 fpath = lambda x: os.path.join(GHROOT, x)
 
-# example, prints the README at the root of the project
-print(open(fpath('README.md')).read())
+# test, prints the README at the root of the project
+def __testpaths():
+    for x in os.listdir(GHROOT):
+        print(x)

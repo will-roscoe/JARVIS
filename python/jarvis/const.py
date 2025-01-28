41797b7bf4b9bf7142b17ca0ca32cfcd312f12f9
Will Roscoe <w.roscoe@lancaster.ac.uk>
2025-01-28 22:52:19 +0000

    new object to use for getting file info from fits names

--- python/jarvis/const.py
+++ python/jarvis/const.py
@@ -15,3 +15,50 @@ def __testpaths():
     for x in os.listdir(GHROOT):
         print(x)
         
+class fileInfo():
+    strOnly = False
+    def __init__(self, rel_path:str,):
+        self._rel_path = rel_path
+        if not os.path.exists(fpath(rel_path)):
+            raise FileNotFoundError(f'File {rel_path} not found.')
+        self._filename = os.path.split(rel_path)[1]
+        self._basename = os.path.basename(rel_path)
+        split = self._basename.split('_')
+        split = [split[0]] + split[1].split('-') + split[2:]
+        keys = ['observation', 'year','days','hours','minutes','seconds', 'exposuretime','visit','instrument','filter','type']
+        self._dict = {k:v for k,v in zip(keys, split)}
+        self._dict['year']= "20" + self._dict['year']
+        if not self.strOnly:
+            for k,v in self._dict.items():
+                if v.isnumeric():
+                    self._dict[k] = int(v)
+            self._dict['visit'] = int(self._dict['visit'][1:])
+            
+            
+
+    def __getattribute__(self, _name):
+        name = _name.lower()
+        if name in self._dict.keys():
+            return self._dict[name]
+        else:
+            for k,v in self._dict.items():
+                if k.startswith(name):
+                    return v
+        return object.__getattribute__(self, _name)
+
+    @property
+    def datetime(self):
+        if self.strOnly:
+            return datetime.datetime.strptime(self._dict['year'] + self._dict['days'] + self._dict['hours'] + self._dict['minutes'] + self._dict['seconds'], '%Y%j%H%M%S')
+        return datetime.datetime(self.year, 1, 1, self.hours, self.minutes, self.seconds) + datetime.timedelta(days=self.days)
+    def __getitem__(self, *args):
+        if len(args) == 1:
+            if isinstance(args[0], (int, slice)):
+                return list(self._dict.values())[args[0]]
+            return self.__getattribute__(args[0])
+        return [self.__getattribute__(x) for x in args]
+
+
+#test = fileInfo(r'datasets\HST\v02\jup_16-138-18-48-16_0100_v02_stis_f25srf2_proj.fits')
+#print(test.split)
+#datasets\HST\v01\jup_16-137-23-43-30_0100_v01_stis_f25srf2_proj.fits

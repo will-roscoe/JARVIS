391689031436cbbbf50f21f064f907fa92915fb6
Will Roscoe <w.roscoe@lancaster.ac.uk>
2025-01-28 22:53:02 +0000

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,18 +1,14 @@
 from astropy.io import fits
-from proj_images_og import moind
-from const import fpath
-i,n=0,0
-n = fpath(r'datasets\HST\v02\jup_16-138-18-48-16_0100_v02_stis_f25srf2_proj.fits')
-hdulist = fits.open(n)
-header = hdulist[1].header
-image = hdulist[1].data
-#print(header)
-try:
-    hemis = header['HEMISPH']
-except NameError:
-    hemis = str(input('Input hemisphere manually:  ("north" or "south")  '))
-#filename = str(i)[-51:-5]
-moind(image, header, filename=n, prefix='ocx8', dpi = 300, crop=1, rlim = 90, fixed = 'lon',
-            hemis = hemis, full=True, moonfp=False, photo=n)
-hdulist.close()
-print(f'Image {n} of {str(i)[-51:-5]} created.')
+from jarvis import *
+
+# n = fpath(r'datasets\HST\v02\jup_16-138-18-48-16_0100_v02_stis_f25srf2_proj.fits')
+# hdulist = fits.open(n)
+# header = hdulist[1].header
+# image = hdulist[1].data
+# hemis = header['HEMISPH']
+# moind(image, header, filename=n, prefix='ocx8', dpi = 300, crop=1, rlim = 90, fixed = 'lon',
+#             hemis = hemis, full=True, photo=n)
+# hdulist.close()
+# print(f'Image {n} created.')
+
+multigif(['v01'],'2016','', '', '100', '40', True, 'lon', )

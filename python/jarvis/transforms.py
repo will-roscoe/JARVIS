e314535d0c5c969e318309298ba1f918597312cb
Will Roscoe <w.roscoe@lancaster.ac.uk>
2025-02-04 15:47:44 +0000

    started on coadd

--- python/jarvis/transforms.py
+++ python/jarvis/transforms.py
@@ -16,3 +16,9 @@ Gx = [[-1  0  1]    Gy = [[-1 -2 -1]                      G= [[-1-1j 0-2j 1-1j]
 def gradmap(input_arr:np.ndarray, kernel2d:np.array=np.array([[-1-1j,-2j,1-1j],[-2,0,2],[-1+1j,2j,1+1j]]),boundary:str='wrap',mode:str='same')->np.ndarray:
       complexret = scipy.signal.convolve2d(input_arr, kernel2d, mode='same', boundary='wrap')
       return np.abs(complexret)
+
+
+def coadd(input_arrs:List[np.ndarray], weights:Optional[List[float]]=None)->np.ndarray:
+    '''Coadd a list of arrays with optional weights.
+    '''
+    raise NotImplementedError('coadd not implemented yet')

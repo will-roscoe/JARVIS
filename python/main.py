796d9337c267d77bf1ae58a7a927c1c05a6da919
Yeefhan <yifan2004c@gmail.com>
2025-02-04 11:43:59 +0000

    v06 long exposure fix

--- python/main.py
+++ python/main.py
@@ -4,10 +4,10 @@ import glob
 
             
 # makes an image
-#n = fpath(r'datasets\HST\v04\jup_16-140-20-05-39_0100_v04_stis_f25srf2_proj.fits')
+#n = fpath(r'datasets\HST\v04\jup_16-140-20-48-59_0103_v04_stis_f25srf2_proj.fits')
 #moind(n, 'temp')
 # makes a gif
-make_gif('datasets/HST/v06',fixed='lt', dpi=300)
+make_gif('datasets/HST/v06', dpi=300)
 
 #hdu = fits.open(n)
 #d = hdu[1].data

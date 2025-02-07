7654fef21154fd153541f42b95236cdd5ecfc1d7
Will Roscoe <w.roscoe@lancaster.ac.uk>
2025-02-07 22:03:21 +0000

    feature/moon-footprint merge (#60)
    * Update polar.py
    
    * redoing moon footprint code
    
    * Update polar.py
    
    * N/S seperation outside
    
    * lon done
    
    * code simplified
    
    * polsih
    
    * updated tests
    
    * Update mergequeue-main.yml
    
    * Update mergequeue-main.yml
    
    ---------
    
    Co-authored-by: Yeefhan <yifan2004c@gmail.com>

--- python/main.py
+++ python/main.py
@@ -1,12 +1,28 @@
 from astropy.io import fits
+
 from jarvis import make_gif, moind, fpath, gradmap
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib as mpl
 import glob
+
+#norm = mpl.colors.Normalize(vmin=0, vmax=1000)
+
+#cmap = (mpl.colors.ListedColormap(['red', 'green', 'blue', 'cyan'])
+#        .with_extremes(under='black', over='white'))
+#bounds = [150, 300, 450, 600, 750]
+#norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+            
+# makes an image
+#n = fpath(r'datasets\HST\v04\jup_16-140-20-48-59_0103_v04_stis_f25srf2_proj.fits')
+#moind(n, 'temp',fixed='lon', full=True, regions=False,moonfp=True)#cmap=cmap,norm=norm
+
          
 # makes an image
 n = fpath(r'datasets\HST\v11\jup_16-146-16-00-57_2703_v11_stis_f25srf2_proj.fits.longexposure')
 moind(n, 'temp', preproj_func=gradmap)
+
 # makes a gif
-#make_gif('datasets/HST/v06', dpi=300)
+#make_gif('datasets/HST/v04', dpi=300, moonfp=True, remove_temp=False)
 
 #hdu = fits.open(n)
 #d = hdu[1].data

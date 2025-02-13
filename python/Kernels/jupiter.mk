5061ffcf99324b728c5fd9a3ecb78c17de13f172
Will Roscoe <w.roscoe@lancaster.ac.uk>
2025-02-13 16:34:56 +0000

    up

---
+++ python/Kernels/jupiter.mk
@@ -0,0 +1,10 @@
+\begindata
+PATH_VALUES = ('/Users/sarah/OneDrive - Lancaster University/Prog/Python/Galileo/kernels')
+PATH_SYMBOLS = ('A')
+KERNELS_TO_LOAD=(
+'$A/naif0012.tls',
+'$A/mk00062a.tsc',
+'$A/pck00011.tpc',
+'$A/de430.bsp',
+'$A/jup365.bsp',
+\begintext

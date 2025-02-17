c44745099f5edf1e2ad17bf9c6aa6459e55fb6ef
Will Roscoe <w.roscoe@lancaster.ac.uk>
2025-02-17 00:53:28 +0000

    ml work

--- python/mscr.py
+++ python/mscr.py
@@ -1,6 +1,6 @@
 from jarvis.transforms import align_cmls, coadd, gradmap, dropthreshold
 from jarvis.polar import process_fits_file, plot_polar, prepare_fits, fits_from_parent
-from jarvis.utils import fpath, fitsheader
+from jarvis.utils import fpath, fitsheader, ensure_dir, make_filename
 
 from astropy.io import fits
 from glob import glob
@@ -11,18 +11,7 @@ import cmasher as cmr
 import random
 import cv2
 import matplotlib as mpl
-targetpaths = glob(fpath(r'datasets\HST\v06\*.fits'))
-fitsfiles = [fits.open(x) for x in targetpaths]
-print(len(fitsfiles))
-aligned_data = [x[1].data for x in fitsfiles]
-coadded_data = coadd(aligned_data)
-graddat = np.where(gradmap(coadded_data)<200,np.nan,gradmap(coadded_data))#gradmap(coadded_data)#
-coadd2 = coadd([graddat,coadded_data], weights=[1,1])
-procorig = prepare_fits(fitsfiles[0], fixed='LT', full=False)
-proc = process_fits_file(fits_from_parent(procorig, new_data=coadd2))
 
-# import numpy as np
-# from matplotlib import pyplot as plt
 
 def mk_stripped_polar(fits_obj,path=None,**kwargs)->None:
     dpi = kwargs.pop('dpi',300)
@@ -32,7 +21,11 @@ def mk_stripped_polar(fits_obj,path=None,**kwargs)->None:
     facecolor, bg_color,thetaa,fit=  kwargs.pop('facecolor',kwargs.get('bg_color', 'black')),kwargs.pop('bg_color','#00000000'), kwargs.pop('theta_adjust',None), kwargs.pop('len_fit',0)
     # do normal plotting with no decorations, just the circle
     plot_polar(fits_obj, ax,cmap=cmap,nodec=nodec, **kwargs)
-    # adjust the thetalimits to remove the artefacts
+    
+    
+    ####################################################################################################
+    ## THIS bit should likely be replaced by something that can adapt better, see outputs.
+    # # adjust the thetalimits to remove the artefacts
 
     if thetaa is not None: # directly set the offsets
         tl = [np.pi/2+thetaa[0],3*np.pi/2+thetaa[1]]
@@ -43,14 +36,15 @@ def mk_stripped_polar(fits_obj,path=None,**kwargs)->None:
             t = np.radians(4.208+(fit-1)*0.233)
         tl = [np.pi/2+t,3*np.pi/2]
         ax.set_thetalim(tl)
+    ####################################################################################################
     ax.set(facecolor=facecolor)
     fig.patch.set_facecolor(bg_color)
-  
+    
     ax.set_aspect(1)
     ax.set_position( [0, -0.45, 1, 1.8])
     for spine in ax.spines.values():
         spine.set_linewidth(0)
-   
+    
     tempdir = fpath(r"temp/"+f"temp_stripped_{random.randint(0,99999999):0<8}.png")
     fig.savefig(tempdir, dpi=dpi)
     img = cv2.imread(tempdir,cv2.IMREAD_GRAYSCALE)
@@ -70,25 +64,44 @@ def mk_stripped_polar(fits_obj,path=None,**kwargs)->None:
         cv2.imwrite(path, img)
     else:
         return img
+ensure_dir(fpath(r"temp/")) 
+
+
+for i in [f"{m:0>2}" for m in range(1,21)]:
+    path = fpath(f'datasets/HST/v{i}/*.fits')
+    print(path)
+    targetpaths = glob(path)
+    fitsfiles = [fits.open(x) for x in targetpaths]
+    print(len(fitsfiles))
+    aligned_data = [x[1].data for x in fitsfiles]
+    coadded_data = coadd(aligned_data)
+    graddat = np.where(gradmap(coadded_data)<70,np.nan,gradmap(coadded_data))#gradmap(coadded_data)#
+    coadd2 = coadd([graddat,coadded_data], weights=[1,1])
+    procorig = prepare_fits(fitsfiles[0], fixed='LT', full=False)
+    proc = process_fits_file(fits_from_parent(procorig, new_data=coadd2))
+
+    # import numpy as np
+    # from matplotlib import pyplot as plt
     
-img = mk_stripped_polar(proc,bg_color='black', facecolor='white', cmap=cmr.neutral,len_fit=32)
+    img = mk_stripped_polar(proc,bg_color='black', facecolor='white', cmap=cmr.neutral_r,len_fit=len(fitsfiles)+5)
+    cv2.imwrite(fpath(f"pictures/bw/{make_filename(proc)}_bw_stripped.jpg"), img)
 impath = fpath(r"pictures\gifs\coadded gradmap&coadd_v06_with200kRgradthresh.png")
-def get_limit_mask(imagearr, path=None)->np.ndarray:
-    print(imagearr.shape[0]) # height
-    # identify the background color and the side that has the facecolor in the top corner
-    bg = imagearr[-1, :]
-    topl,topr = imagearr[0,0],imagearr[0,-1]
-    left,right = imagearr[:imagearr.shape[0]//2, :], imagearr[:imagearr.shape[0]//2,:]
-    testside,fc = [left,topl] if all(topl==bg) else [right,topr]
-    cutoff = 0
-    # test the test side to find the index of the first row
-    for i in range(testside.shape[0]):
-        if not np.all([testside[j,i] == bg or testside[j,i]==fc for j in range(testside.shape[1])]):
-            cutoff = i
-            break
-    cropped = imagearr[cutoff:,:]
-    if path is not None:
-        cv2.imwrite(path, cropped)
-    else:
-        return cropped
-get_limit_mask(img,impath)
+# def get_limit_mask(imagearr, path=None)->np.ndarray:
+#     print(imagearr.shape[0]) # height
+#     # identify the background color and the side that has the facecolor in the top corner
+#     bg = imagearr[-1, :]
+#     topl,topr = imagearr[0,0],imagearr[0,-1]
+#     left,right = imagearr[:imagearr.shape[0]//2, :], imagearr[:imagearr.shape[0]//2,:]
+#     testside,fc = [left,topl] if all(topl==bg) else [right,topr]
+#     cutoff = 0
+#     # test the test side to find the index of the first row
+#     for i in range(testside.shape[0]):
+#         if not np.all([testside[j,i] == bg or testside[j,i]==fc for j in range(testside.shape[1])]):
+#             cutoff = i
+#             break
+#     cropped = imagearr[cutoff:,:]
+#     if path is not None:
+#         cv2.imwrite(path, cropped)
+#     else:
+#         return cropped
+# get_limit_mask(img,impath)

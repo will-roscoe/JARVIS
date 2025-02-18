3412f0d365f6fc4d2a104bbddce0dec92c3af531
RonanSzeto <r.szeto@lancaster.ac.uk>
2025-02-18 13:37:40 +0000

    commit

---
+++ python/sw_plot.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from pathlib import Path
+GHROOT = Path(__file__).parents[1]
+def fpath(x):
+    return os.path.join(GHROOT, x)
+
+pd.options.display.max_rows = 9999
+
+filename = r"datasets\Solar_Wind.txt"
+table = pd.read_csv(fpath(filename), header=0,sep=r'\s+')
+print(table.columns)
+
+table['Time_UT'] = pd.to_datetime(table['Time_UT'], format='%Y-%m-%d-%H:%M:%S.%f') # see https://strftime.org/ for format codes
+# plot the table:
+table.plot(x='Time_UT', y='jup_sw_pdyn', title='Solar wind pressure vs Time at Jupiter', xlabel='Time_UT', ylabel='jup_sw_pdyn')
+
+plt.show()

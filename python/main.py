b90ebf16e8222b521da3516ccf679b5b9bcd7613
Will Roscoe <w.roscoe@lancaster.ac.uk>
2025-03-02 22:55:18 +0000

    added examples of segmented groups

--- python/main.py
+++ python/main.py
@@ -1,5 +1,7 @@
 
 
+import datetime
+import os
 import matplotlib as mpl
 mpl.use('qtagg') # forces the use of the Qt5/6 backend, neccessary for pathfinder
 from jarvis import fpath
@@ -37,64 +39,86 @@ import pandas as pd #noqa: F401
 #print(d)
 
 
+outfile = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.txt'
 # NOTE: we use this condition to run the code only if it is run as a script to avoid running it when imported, this is best practice.
 if __name__ == '__main__': # __name__ is a special,file-unique variable that is set to '__main__' when the script is run as a script, and set to the name of the module when imported.
-    gps = [1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+    gps = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     #remove 3 (broken), 20 (southern) #group Numbe
 # loop to generate the coadded fits, identify the boundary, and calculate the power
-    for i in tqdm(gps):
-        basefitpath = fpath(f'datasets/HST/group_{i:0>2}') 
-        fitsg = fits_from_glob(basefitpath)
-        copath = fpath(f'datasets/HST/custom/g{i+1:0>2},v{group_to_visit(i+1):0>2}_[3,1]gaussian-coadded.fits')
-        fit = generate_coadded_fits(fitsg, saveto=copath, gaussian=(3,1), overwrite=True,indiv=False, coadded=True)
-        #fit.info()
-        fit.close()
-        pt = pathfinder(copath)
-        fit = fits.open(copath)
-        #fit.info()
-        # print(*[fi for fi in f], sep='\n')
-        #print(*[f.fileinfo(i) for i in range(len(f))], sep='\n')
-        #f.info()
-        try:
-            path = np.array(fit['BOUNDARY'].data.tolist())
-            fit.close()
-            pbr = tqdm(total=len(fitsg), desc=f'"powercalc"(group {i} of {len(gps)})')
-            for j,f in enumerate(fitsg):
-                pc = powercalc(f,path)
-                pbr.set_postfix_str(f"P={pc[0]:.3f}GW,I={pc[1]*1e8:.3f}x10⁻⁸GW/km²")
-                pbr.update()
-                f.close()
-            pbr.close()
-        except KeyError:
-            fit.close()
-            print(f'No boundary found for group {i}')
-# # loop to only generate the gaussians ----------------------------------------
+    # for i in tqdm(gps):
+    #     basefitpath = fpath(f'datasets/HST/group_{i:0>2}') 
+    #     fitsg = fits_from_glob(basefitpath)
+    #     copath = fpath(f'datasets/HST/custom/g{i+1:0>2},v{group_to_visit(i+1):0>2}_[3,1]gaussian-coadded.fits')
+    #     fit = generate_coadded_fits(fitsg, saveto=copath, gaussian=(3,1), overwrite=True,indiv=False, coadded=True)
+    #     #fit.info()
+    #     fit.close()
+    #     pt = pathfinder(copath)
+    #     fit = fits.open(copath)
+    #     #fit.info()
+    #     # print(*[fi for fi in f], sep='\n')
+    #     #print(*[f.fileinfo(i) for i in range(len(f))], sep='\n')
+    #     #f.info()
+    #     try:
+    #         path = np.array(fit['BOUNDARY'].data.tolist())
+    #         fit.close()
+    #         pbr = tqdm(total=len(fitsg), desc=f'"powercalc"(group {i} of {len(gps)})')
+    #         for j,f in enumerate(fitsg):
+    #             pc = powercalc(f,path)
+    #             pbr.set_postfix_str(f"P={pc[0]:.3f}GW,I={pc[1]*1e8:.3f}x10⁻⁸GW/km²")
+    #             pbr.update()
+    #             f.close()
+    #         pbr.close()
+    #     except KeyError:
+    #         fit.close()
+    #         print(f'No boundary found for group {i}')
+# loop to only generate the gaussians ----------------------------------------
 #     for i in tqdm(gps):
 #         basefitpath = fpath(f'datasets/HST/group_{i:0>2}') 
 #         fitsg = fits_from_glob(basefitpath)
-#         copath = fpath(f'datasets/HST/custom/g{i+1:0>2},v{group_to_visit(i+1):0>2}_[3,1]gaussian-coadded.fits')
-#         fit = gaussian_coadded_fits(fitsg, saveto=copath, gaussian=(3,1), overwrite=True,indiv=False, coadded=True)
+#         copath = fpath(f'datasets/HST/custom/g{i:0>2},v{group_to_visit(i):0>2}_[3,1]gaussian-coadded.fits')
+#         fit = generate_coadded_fits(fitsg, saveto=copath, gaussian=(3,1), overwrite=True,indiv=False, coadded=True)
 #         fit.close()
 # # loop to only run pathfinder ------------------------------------------------
-#     for i in tqdm(gps):
-#         copath = fpath(f'datasets/HST/custom/g{i+1:0>2},v{group_to_visit(i+1):0>2}_[3,1]gaussian-coadded.fits')
-#         pt = pathfinder(copath)
-# # loop to only run powercalc -------------------------------------------------
-#     for i in tqdm(gps):
-#         basefitpath = fpath(f'datasets/HST/group_{i:0>2}') 
-#         fitsg = fits_from_glob(basefitpath)
-#         copath = fpath(f'datasets/HST/custom/g{i+1:0>2},v{group_to_visit(i+1):0>2}_[3,1]gaussian-coadded.fits')
-#         fit = fits.open(copath)
-#         path = np.array(fit['BOUNDARY'].data.tolist())
-#         fit.close()
-#         pbr = tqdm(total=len(fitsg), desc=f'"powercalc"(group {i} of {len(gps)})')
-#         for j,f in enumerate(fitsg):
-#             pc = powercalc(f,path)
-#             pbr.set_postfix_str(f"P={pc[0]:.3f}GW,I={pc[1]*1e8:.3f}x10⁻⁸GW/km²")
-#             pbr.update()
-#             f.close()
-#         pbr.close()
-# #<----------------------------------------------------------------------------
+    # for i in tqdm(gps):
+    #     copath = fpath(f'datasets/HST/custom/g{i:0>2},v{group_to_visit(i):0>2}_[3,1]gaussian-coadded.fits')
+    #     pt = pathfinder(copath)
+# loop to only run powercalc -------------------------------------------------
+    # for i in tqdm(gps):
+    #     basefitpath = fpath(f'datasets/HST/group_{i:0>2}') 
+    #     fitsg = fits_from_glob(basefitpath)
+    #     copath = fpath(f'datasets/HST/custom/g{i:0>2},v{group_to_visit(i):0>2}_[3,1]gaussian-coadded.fits')
+    #     fit = fits.open(copath)
+    #     try:
+    #         path = np.array(fit['BOUNDARY'].data.tolist())
+    #         fit.close()
+    #         pbr = tqdm(total=len(fitsg), desc=f'"powercalc"(group {i} of {len(gps)})')
+    #         for j,f in enumerate(fitsg):
+    #             pc = powercalc(f,path, writeto=outfile)
+    #             pbr.set_postfix_str(f"P={pc[0]:.3f}GW,I={pc[1]*1e8:.3f}x10⁻⁸GW/km²")
+    #             pbr.update()
+    #             f.close()
+    #         pbr.close()
+    #     except KeyError:
+    #         fit.close()
+    #         print(f'No boundary found for group {i}')
+## loop to generate gaussians of segments of the fits
+    fpaths = hst_segmented_paths(2,False)
+# pbar = tqdm(total=len(fpaths), desc='Generating coadded fits')
+# for g,f in fpaths.items():
+#         if any(f'g{grp:0>2}' in g for grp in gps):
+#             copath = fpath(f'datasets/HST/custom/{g}_[3,1]gaussian-coadded.fits')
+#             print(f)
+#             fit = generate_coadded_fits([fits.open(ff) for ff in f], saveto=copath, gaussian=(3,1), overwrite=True,indiv=False, coadded=True)
+#             fit.close()
+#         pbar.update()
+# pbar.close()
+## loop to run pathfinder on segments of the fits
+    lrange = (0.25,0.32)
+
+    for g in fpaths.keys():
+        copath =fpath(f'datasets/HST/custom/{g}_[3,1]gaussian-coadded.fits')
+        if os.path.isfile(copath):
+            pt = pathfinder(copath, fixlrange=lrange)
 
 
 
